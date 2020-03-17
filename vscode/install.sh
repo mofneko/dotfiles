08ada5d4aa5358e20248e26606f8cc34bcf0bdf9
Yusuke Arakawa <mofneko@users.noreply.github.com>
2020-03-17 12:05:00 +0900

    Add VS Code .conf files.

---
+++ vscode/install.sh
@@ -0,0 +1,12 @@
+code --install-extension teabyii.ayu
+code --install-extension Dart-Code.dart-code
+code --install-extension Dart-Code.flutter
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension redhat.vscode-yaml
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension eamodio.gitlens
+code --install-extension jebbs.plantuml
